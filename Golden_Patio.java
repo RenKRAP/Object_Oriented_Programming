6dfa486c499f933a3fbd661d3c458833d4b4aaad
Ujin <131636662+RenKRAP@users.noreply.github.com>
2026-09-01 16:23:20 +0900

    Add files via upload

---
+++ Golden_Patio.java
@@ -0,0 +1,16 @@
+package OPP_HW;
+
+public class Golden_Patio {
+	public static void main(String[] args) {
+		int a=1, b=1, c;
+		double ratio;
+		
+		for(int i=0; i<20; i++) {
+			c=a+b;
+			ratio=(double)c/b;
+			System.out.printf("%.12f\n", ratio);
+			a=b;
+			b=c;
+		}
+	}
+}
