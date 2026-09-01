91baa8bdd875bb3ad3b94c774378292933d6d42c
Ujin <131636662+RenKRAP@users.noreply.github.com>
2026-09-01 15:33:02 +0900

    Add files via upload

---
+++ triangle.java
@@ -0,0 +1,46 @@
+class triangle {
+    public static void main(String[] args) {
+        for (int i=0; i<10; i++){
+            for(int j=0; j<=i; j++){
+                System.out.print("#");
+            }
+            for(int j=i+1; j<10;j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        for (int i=0; i<10; i++){
+            for(int j=i; j<10; j++){
+                System.out.print("#");
+            }
+            for(int j=0; j<=i-1; j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        for (int i=0; i<10; i++){
+            for(int j=i+1; j<10; j++){
+                System.out.print(" ");
+            }
+            for(int j=0; j<=i; j++){
+                System.out.print("#");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        for (int i=0; i<10; i++){
+            for(int j=0; j<i; j++){
+                System.out.print(" ");
+            }
+            for(int j=i; j<10; j++){
+                System.out.print("#");
+            }
+            System.out.println();
+        }    
+    }
+}
